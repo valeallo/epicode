f515e2e16f822a0e628c188b54dba1a69cceb07a
valentina.allodi <valentina.allodi@gmail.com>
2022-07-25 00:50:30 +0200

    XX

--- M2/D8/script2.js
+++ M2/D8/script2.js
@@ -112,3 +112,136 @@ function change_title(newTitle) {
     document.getElementsByTagName("title")[0].innerHTML = newTitle;
 
 }
+
+//35) Scrivi una funzione per aggiungere una nuova riga alla tabella nella pagina
+function add_row(text0 = "default", text1 = "default", text2 = "default", text3 = "default", text4 = "default") {
+    let table = document.getElementById("myTable");
+    let row = table.insertRow();
+    let cell0 = row.insertCell(0);
+    let cell1 = row.insertCell(1);
+    let cell2 = row.insertCell(2);
+    let cell3 = row.insertCell(3);
+    let cell4 = row.insertCell(4);
+    cell0.innerHTML = text0;
+    cell1.innerHTML = text1;
+    cell2.innerHTML = text2;
+    cell3.innerHTML = text3;
+    cell4.innerHTML = text4;
+}
+//36) Scrivi una funzione per aggiungere la classe "test" ad ogni riga nella tabella
+function add_class_test() {
+    let tr = document.getElementsByTagName("tr")
+    for (let el of tr) {
+        el.classList.add("test");
+    }
+}
+
+//37) Scrivi una funzione per aggiungere uno sfondo rosso a ogni link presente nella pagina
+
+function add_background() {
+    let a = document.getElementsByTagName("a")
+    for (el of a) {
+        el.style.backgroundColor = "red";
+    }
+}
+//38) Mostra in console "Page loaded" quando la pagina ha finito di caricarsi
+
+window.addEventListener("load", function () {
+    console.log("page loaded");
+
+})
+
+
+//39) Scrivi una funzione per aggiungere nuovi elementi alla lista non ordinata nella pagina
+function add_to_ul(list_element) {
+    let ul = document.getElementsByTagName("ul")[0];
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(list_element));
+    ul.appendChild(li);
+}
+
+
+
+//40) Scrivi una funzione per svuotare una lista nella pagina
+
+
+function empty_list() {
+    let myList = document.getElementById("ordered_list");
+    let items = document.querySelectorAll("#ordered_list li");
+    for (let i = 0; i < items.length; i++) {
+        myList.removeChild(items[i]);
+    }
+}
+
+//EXTRAS 
+//41) Aggiungi un event listener in modo che compaia un alert quando il cursore passa sopra un link; l'alert deve mostrare la sua proprietà href
+document.getElementsByTagName("a")[0].addEventListener("mouseover", () => {
+    alert(document.getElementsByTagName("a")[0].href)
+})
+
+
+//42) Crea un bottone che una volta cliccato nasconde ogni immagine nella pagina
+function hide(element) {
+    element.classList.toggle("hidden");//ho messo toggle perché mi pare più bellino far scomparire e riapparire ma all'inizio avevo messo add
+}
+let deletePicsBtn = document.createElement("button");
+deletePicsBtn.innerHTML = "Delete Pics";
+document.body.appendChild(deletePicsBtn);
+
+deletePicsBtn.onclick = function () {
+    let imgs = document.getElementsByTagName("img");
+    for (let el of imgs) {
+        hide(el);
+    }
+}
+//43) Crea un bottone che una volta cliccato nasconde o mostra la tabella nella pagina
+
+let table = document.getElementById("myTable");
+let deleteTableBtn = document.createElement("button");
+deleteTableBtn.innerHTML = "Toggle Table";
+document.body.appendChild(deleteTableBtn);
+
+deleteTableBtn.onclick = function () {
+    hide(table);
+};
+
+
+//44) Crea una funzione per calcolare la somma di ogni numero contenuto in tutte le celle della tabella(se il contenuto è un numero)
+let tds = document.getElementsByTagName("td");
+
+function count_cells() {
+    let total = 0;
+    for (let i = 0; i < tds.length; i++) {
+        let td_content = parseInt(tds[i].innerHTML);
+       
+        if (isNaN(td_content) == false) {
+            total += td_content;
+        }
+        else {
+            continue;
+        }
+    }
+    return total;
+}
+
+//45) Cancella l'ultima lettera dal titolo della pagina ogni volta che l'utente ci clicca sopra
+
+
+
+//46) Aggiungi un event listener in modo che cliccando un < td > nella pagina, questo cambi colore di sfondo
+
+
+
+//47) Aggiungi un bottone che una volta cliccato elimini un < td > a caso nella tabella
+
+
+
+//48) Aggiungi automaticamente un bordo rosa ad una cella della tabella quando il cursore ci passa sopra
+
+
+
+//49) Scrivi una funzione per creare una tabella con 4 righe e 3 colonne e aggiungerla alla fine della pagina
+
+
+
+//50) Scrivi una funzione per rimuovere l'ultima tabella presente nella pagina
